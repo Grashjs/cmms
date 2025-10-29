53d3574c051b294b71667b8bc4aa5cc90ffc35e8
amb00u <ambroise.tine@gmail.com>
2025-10-29 23:59:25 +0100

    test: needed for AssertService unit test

---
+++ api/src/main/java/com/grash/dto/VendorShowDTO.java
@@ -0,0 +1,13 @@
+package com.grash.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class VendorShowDTO {
+    private Long id;
+    private String name;
+}
