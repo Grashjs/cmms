74ead4816f7f1324d52a6cb46758a29259bd5743
Ibrahima G. Coulibaly <ibracool99@gmail.com>
2025-12-02 01:18:33 +0100

    feat: detect browser language

--- frontend/src/i18n/i18n.ts
+++ frontend/src/i18n/i18n.ts
@@ -1,7 +1,6 @@
 import i18n from 'i18next';
-
 import { initReactI18next } from 'react-i18next';
-// import LanguageDetector from 'i18next-browser-languagedetector';
+import LanguageDetector from 'i18next-browser-languagedetector';
 import deJSON from './translations/de';
 import arJSON from './translations/ar';
 import locale from './translations/en';
@@ -26,8 +25,7 @@ import {
   IT,
   SE,
   RU
-} 
-from 'country-flag-icons/react/3x2';
+} from 'country-flag-icons/react/3x2';
 import { LocaleSingularArg } from '@fullcalendar/react';
 import deLocale from '@fullcalendar/core/locales/de';
 import esLocale from '@fullcalendar/core/locales/es';
@@ -41,15 +39,26 @@ import itLocale from '@fullcalendar/core/locales/it';
 import svLocale from '@fullcalendar/core/locales/sv';
 import ruLocale from '@fullcalendar/core/locales/ru';
 import { Locale as DateLocale } from 'date-fns';
-import { fr, de, es, ar, tr, ptBR, pl, enUS, it, sv, ru } from 'date-fns/locale';
+import {
+  fr,
+  de,
+  es,
+  ar,
+  tr,
+  ptBR,
+  pl,
+  enUS,
+  it,
+  sv,
+  ru
+} from 'date-fns/locale';
+
 const resources = {
   de: { translation: deJSON },
   en: { translation: locale },
   es: { translation: esJSON },
   fr: { translation: frJSON },
   pl: { translation: plJSON },
-  // ae: { translation: aeJSON },
-  // cn: { translation: zhJSON },
   tr: { translation: trJSON },
   pt_br: { translation: ptBRJSON },
   ar: { translation: arJSON },
@@ -59,18 +68,23 @@ const resources = {
 };
 
 i18n
-  // .use(LanguageDetector)
+  .use(LanguageDetector)
   .use(initReactI18next)
   .init({
     resources,
+    supportedLngs: Object.keys(resources),
     keySeparator: false,
-    lng: 'en',
     fallbackLng: 'en',
     react: {
       useSuspense: true
     },
     interpolation: {
       escapeValue: false
+    },
+    detection: {
+      lookupLocalStorage: 'lang',
+      caches: ['localStorage'],
+      convertDetectedLanguage: (lng) => lng.split('-')[0].toLowerCase()
     }
   });
 
@@ -88,7 +102,7 @@ export type SupportedLanguage =
   | 'AR';
 
 export const supportedLanguages: {
-  code: string;
+  code: Lowercase<SupportedLanguage>;
   label: string;
   Icon: FlagComponent;
   calendarLocale: LocaleSingularArg;
@@ -172,4 +186,5 @@ export const supportedLanguages: {
     dateLocale: ru
   }
 ];
+
 export default i18n;
