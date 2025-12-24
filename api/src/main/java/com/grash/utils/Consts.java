5f9571e3cbd7326aef75fec7939f5935421a914f
Ibrahima G. Coulibaly <ibracool99@gmail.com>
2025-12-24 23:23:45 +0100

    fix: license names

--- api/src/main/java/com/grash/utils/Consts.java
+++ api/src/main/java/com/grash/utils/Consts.java
@@ -16,7 +16,7 @@ public class Consts {
             SelfHostedPlan.builder()
                     .id("sh-professional-monthly")
                     .pricePerUser(BigDecimal.valueOf(15))
-                    .name("Professional")
+                    .name("Professional Atlas CMMS license")
                     .monthly(true)
                     .keygenPolicyId("5df4c975-8933-4c9f-89e0-2207365699a9")
                     .build(),
@@ -25,7 +25,7 @@ public class Consts {
                     .pricePerUser(BigDecimal.valueOf(100))
                     .keygenPolicyId("c168a294-7f62-47bc-a010-a26e8758b00c")
                     .monthly(true)
-                    .name("Enterprise")
+                    .name("Enterprise Atlas CMMS license")
                     .build()
     );
 }
