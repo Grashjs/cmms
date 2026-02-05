7cc7695045ff462d9f1613609ed49041e9608594
Ibrahima G. Coulibaly <ibracool99@gmail.com>
2026-02-06 00:53:02 +0100

    fix: disable company logos and testimonials sections in IndustryLayout

--- frontend/src/layouts/IndustryLayout/index.tsx
+++ frontend/src/layouts/IndustryLayout/index.tsx
@@ -136,7 +136,7 @@ const IndustryLayout: FC<IndustryLayoutProps> = (props) => {
         </Container>
 
         {/* Company Logos */}
-        {companyLogos && (
+        {false && companyLogos && (
           <Container maxWidth="lg" sx={{ py: 5 }}>
             <Grid container spacing={4} justifyContent="center">
               {companyLogos.map((logo, index) => (
@@ -291,33 +291,35 @@ const IndustryLayout: FC<IndustryLayoutProps> = (props) => {
         </Container>
 
         {/* Testimonials */}
-        <Box
-          sx={{
-            py: 4
-          }}
-        >
-          <Container maxWidth="lg">
-            <Typography variant="h2" align="center" mb={3}>
-              Hear it from our customers
-            </Typography>
-            <Grid container spacing={4}>
-              {testimonials.map((testimonial, index) => (
-                <Grid item xs={12} md={6} key={index}>
-                  <Card>
-                    <CardContent>
-                      <Typography variant="body1" paragraph>
-                        "{testimonial.text}"
-                      </Typography>
-                      <Typography variant="subtitle1" align="right">
-                        - {testimonial.author}, {testimonial.company}
-                      </Typography>
-                    </CardContent>
-                  </Card>
-                </Grid>
-              ))}
-            </Grid>
-          </Container>
-        </Box>
+        {false && (
+          <Box
+            sx={{
+              py: 4
+            }}
+          >
+            <Container maxWidth="lg">
+              <Typography variant="h2" align="center" mb={3}>
+                Hear it from our customers
+              </Typography>
+              <Grid container spacing={4}>
+                {testimonials.map((testimonial, index) => (
+                  <Grid item xs={12} md={6} key={index}>
+                    <Card>
+                      <CardContent>
+                        <Typography variant="body1" paragraph>
+                          "{testimonial.text}"
+                        </Typography>
+                        <Typography variant="subtitle1" align="right">
+                          - {testimonial.author}, {testimonial.company}
+                        </Typography>
+                      </CardContent>
+                    </Card>
+                  </Grid>
+                ))}
+              </Grid>
+            </Container>
+          </Box>
+        )}
 
         {/* FAQ */}
         <Container maxWidth="lg">
