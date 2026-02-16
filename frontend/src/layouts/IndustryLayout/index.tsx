4d6970a3a16465905ebb55dd6e0a848544ad4321
Ibrahima G. Coulibaly <ibracool99@gmail.com>
2026-02-16 01:50:27 +0000

    fix: refactor testimonials into reusable component and update IndustryLayout

--- frontend/src/layouts/IndustryLayout/index.tsx
+++ frontend/src/layouts/IndustryLayout/index.tsx
@@ -23,18 +23,15 @@ import { SvgIconComponent } from '@mui/icons-material';
 import TwoCallToActions from '../../content/landing/components/TwoCallToActions';
 import { companyLogosAssets } from '../../utils/overall';
 import CompanyLogos from '../../content/landing/components/CompanyLogos';
+import Testimonials, {
+  Testimonial
+} from '../../content/landing/components/Testimonials';
 
 interface Feature {
   title: string;
   description: string;
   imageUrl: string;
   learnMoreUrl?: string;
-}
-
-interface Testimonial {
-  text: string;
-  author: string;
-  company: string;
 }
 
 interface FAQ {
@@ -300,34 +297,8 @@ const IndustryLayout: FC<IndustryLayoutProps> = (props) => {
         </Container>
 
         {/* Testimonials */}
-        {false && (
-          <Box
-            sx={{
-              py: 4
-            }}
-          >
-            <Container maxWidth="lg">
-              <Typography variant="h2" align="center" mb={3}>
-                Hear it from our customers
-              </Typography>
-              <Grid container spacing={4}>
-                {testimonials.map((testimonial, index) => (
-                  <Grid item xs={12} md={6} key={index}>
-                    <Card>
-                      <CardContent>
-                        <Typography variant="body1" paragraph>
-                          "{testimonial.text}"
-                        </Typography>
-                        <Typography variant="subtitle1" align="right">
-                          - {testimonial.author}, {testimonial.company}
-                        </Typography>
-                      </CardContent>
-                    </Card>
-                  </Grid>
-                ))}
-              </Grid>
-            </Container>
-          </Box>
+        {testimonials?.length > 0 && (
+          <Testimonials testimonials={testimonials} />
         )}
         <TwoCallToActions />
         {/* FAQ */}
