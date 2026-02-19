3c3c1425c5cf05995948c2fabc3c7106a1e27cab
Ibrahima G. Coulibaly <ibracool99@gmail.com>
2026-02-19 20:09:55 +0000

    fix: increase default user limit to 300 for cloud subscriptions

--- api/src/main/java/com/grash/service/UserService.java
+++ api/src/main/java/com/grash/service/UserService.java
@@ -154,7 +154,7 @@ public class UserService {
             if (!licenseService.hasEntitlement(LicenseEntitlement.MULTI_INSTANCE) && companyService.existsAtLeastOneWithMinWorkOrders())
                 throw new CustomException("You need a license to create another company", HttpStatus.FORBIDDEN);
             Subscription subscription =
-                    Subscription.builder().usersCount(cloudVersion ? 50 : 100).monthly(cloudVersion)
+                    Subscription.builder().usersCount(300).monthly(cloudVersion)
                             .startsOn(new Date())
                             .endsOn(cloudVersion ? Helper.incrementDays(new Date(), 15) : null)
                             .subscriptionPlan(subscriptionPlanService.findByCode("BUSINESS").get()).build();
