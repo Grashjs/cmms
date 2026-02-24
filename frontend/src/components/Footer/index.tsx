1c3cb54fa0195ee126236e10b013e897a8619dee
Ibrahima G. Coulibaly <ibracool99@gmail.com>
2026-02-24 06:29:09 +0000

    refactor: wrap Footer images in `<a>` for improved accessibility and proper external linking

--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -246,14 +246,14 @@ export function Footer() {
         return (
           <Stack spacing={1} direction={{ xs: 'column', lg: 'row' }}>
             {section.items.map((item, index) => (
-              <img
+              <a
                 key={index}
-                style={{ cursor: 'pointer' }}
-                onClick={() => (window.location.href = item.href)}
-                width="150px"
-                src={item.image}
-                alt={item.alt}
-              />
+                href={item.href}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img width="150" src={item.image} alt={item.alt} />
+              </a>
             ))}
           </Stack>
         );
