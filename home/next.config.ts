f56567dcbdb5140486d7a83275181ea81f0107a6
Ibrahima G. Coulibaly <ibracool99@gmail.com>
2026-02-27 01:33:23 +0000

    chore: code inspector

--- home/next.config.ts
+++ home/next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
+import { codeInspectorPlugin } from 'code-inspector-plugin';
 
 const nextConfig: NextConfig = {
   /* config options here */
+    turbopack: {
+        rules: codeInspectorPlugin({
+            bundler: 'turbopack',
+            editor: 'idea',
+            hotKeys: ['altKey'],
+        }),
+    },
 };
 
 export default nextConfig;
