d040a890f94b892764b8bbfdc457ce84a1cedd80
Ibrahima G. Coulibaly <ibracool99@gmail.com>
2026-02-27 05:52:06 +0000

    chore: company logos

--- home/src/content/overview/index.tsx
+++ home/src/content/overview/index.tsx
@@ -1,13 +1,14 @@
 "use client";
 
-import {Box, styled} from '@mui/material';
-import {useRouter} from 'next/navigation';
-import { useTranslations } from 'next-intl';
-import Hero from './Hero';
-import Highlights from './Highlights';
-import NavBar from 'src/components/NavBar';
-import {useBrand} from 'src/hooks/useBrand';
-import {Footer} from 'src/components/Footer';
+import { Box, styled } from "@mui/material";
+import { useRouter } from "next/navigation";
+import { useTranslations } from "next-intl";
+import Hero from "./Hero";
+import Highlights from "./Highlights";
+import NavBar from "src/components/NavBar";
+import { useBrand } from "src/hooks/useBrand";
+import { Footer } from "src/components/Footer";
+import CompanyLogos from "src/components/CompanyLogos";
 
 const OverviewWrapper = styled(Box)(
   ({ theme }) => `
@@ -15,7 +16,7 @@ const OverviewWrapper = styled(Box)(
     background: ${theme.palette.common.white};
     flex: 1;
     overflow-x: hidden;
-`
+`,
 );
 function Overview() {
   const t = useTranslations();
@@ -26,6 +27,7 @@ function Overview() {
     <OverviewWrapper>
       <NavBar />
       <Hero />
+      <CompanyLogos sx={{ mt: { xs: "150px", md: "100px" } }} />
       <Highlights />
       <Footer />
     </OverviewWrapper>
@@ -33,4 +35,3 @@ function Overview() {
 }
 
 export default Overview;
-
--- home/src/utils/overall.ts
+++ home/src/utils/overall.ts
@@ -13,130 +13,15 @@ import { googleTrackingId, IS_LOCALHOST } from '../config';
 import ReactGA from 'react-ga4';
 import { UaEventOptions } from 'react-ga4/types/ga4';
 
-export const canAddReading = (meter: Meter): boolean => {
-  if (!meter) {
-    return false;
-  }
-  if (!meter.nextReading) return true;
-
-  return new Date() >= new Date(meter.nextReading);
-};
-
-export const getImageAndFiles = (
-  files: { id: number; type: FileType }[],
-  imageFallback?
-) => {
-  return {
-    image: files.find((file) => file.type === 'IMAGE')
-      ? { id: files.find((file) => file.type === 'IMAGE').id }
-      : imageFallback ?? null,
-    files: files
-      .filter((file) => file.type === 'OTHER')
-      .map((file) => {
-        return { id: file.id };
-      })
-  };
-};
-
-export const getNextOccurence = (date: Date, days: number): Date => {
-  const incrementDays = (date: Date, days: number) => {
-    date.setDate(date.getDate() + days);
-    return date;
-  };
-  let result = date;
-  if (result > new Date()) {
-    result = incrementDays(result, days);
-  } else
-    while (result < new Date()) {
-      result = incrementDays(result, days);
-    }
-  return result;
-};
-
-export const getRandomColor = () => {
-  var letters = '0123456789ABCDEF';
-  var color = '#';
-  for (var i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)];
-  }
-  return color;
-};
-
-export const getType = (
-  object: Object
-): 'boolean' | 'string' | 'date' | 'number' | 'array' => {
-  if (check.boolean(object)) return 'boolean';
-  if (check.string(object)) return 'string';
-  if (check.date(object)) return 'date';
-  if (check.number(object)) return 'number';
-  if (check.array(object)) return 'array';
-};
-
-export type SpreadsheetData = {
-  [key: string]: Matrix<CellBase<any>>[];
-};
-export const arrayToAoA = (array: string[][]): SpreadsheetData => {
-  const headers = array[0];
-  array.shift();
-  const result = {};
-  headers.forEach((header, index) => {
-    const values = [];
-    array.forEach((arr) => values.push(arr[index]));
-    result[header] = [values.map((value) => [{ value, readOnly: true }])];
-  });
-  return result;
-};
-
-export const pushOrRemove = (array: string[], push: boolean, value: string) => {
-  if (push) {
-    array.push(value);
-  } else {
-    const index = array.findIndex((element) => element === value);
-    if (index !== -1) array.splice(index, 1);
-  }
-  return array;
-};
-
-export const onSearchQueryChange = <T>(
-  event,
-  criteria: SearchCriteria,
-  setCriteria: React.Dispatch<React.SetStateAction<SearchCriteria>>,
-  fieldsToSearch: Extract<keyof T, string>[]
-) => {
-  const query = event.target.value;
-  let newFilterFields: FilterField[] = [...criteria.filterFields];
-
-  newFilterFields = newFilterFields.filter(
-    // @ts-ignore
-    (filterField) => !fieldsToSearch.includes(filterField.field)
-  );
-  const firstField = fieldsToSearch.shift();
-  if (query)
-    newFilterFields = [
-      ...newFilterFields,
-      {
-        field: firstField,
-        value: query,
-        operation: 'cn' as SearchOperator,
-        alternatives: fieldsToSearch.map((field) => ({
-          field,
-          operation: 'cn' as SearchOperator,
-          value: query
-        }))
-      }
-    ];
-  setCriteria({ ...criteria, filterFields: newFilterFields });
-};
-
 export const fireGa4Event = (
-  optionsOrName: UaEventOptions | string,
-  conversionKey?: string,
-  params?: any
+    optionsOrName: UaEventOptions | string,
+    conversionKey?: string,
+    params?: any
 ) => {
   if (
-    !IS_LOCALHOST &&
-    googleTrackingId &&
-    (conversionKey ? !sessionStorage.getItem(conversionKey) : true)
+      !IS_LOCALHOST &&
+      googleTrackingId &&
+      (conversionKey ? !sessionStorage.getItem(conversionKey) : true)
   ) {
     // Fire GA4 event
     ReactGA.event(optionsOrName, params);
@@ -145,9 +30,9 @@ export const fireGa4Event = (
     //@ts-ignore
     if (window.uetq) {
       const eventName =
-        typeof optionsOrName === 'string'
-          ? optionsOrName
-          : optionsOrName.action || optionsOrName.category;
+          typeof optionsOrName === 'string'
+              ? optionsOrName
+              : optionsOrName.action || optionsOrName.category;
       //@ts-ignore
       window.uetq.push('event', eventName, params || {});
     }
@@ -155,3 +40,13 @@ export const fireGa4Event = (
     if (conversionKey) sessionStorage.setItem(conversionKey, 'true');
   }
 };
+
+export const companyLogosAssets: string[] = [
+  '/static/images/industries/logos/adventure-mechanical.png',
+  '/static/images/industries/logos/sertec.png',
+  '/static/images/industries/logos/complete-am.png',
+  '/static/images/industries/logos/kwdc.png',
+  '/static/images/industries/logos/henalux.png',
+  '/static/images/industries/logos/penflex.png',
+  '/static/images/industries/logos/mfwaterwork.png'
+];
