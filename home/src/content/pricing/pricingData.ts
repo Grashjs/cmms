644f2c850c1dc6f356ed8421b1261e7b136a6393
Ibrahima G. Coulibaly <ibracool99@gmail.com>
2026-03-19 03:42:01 +0000

    fix: enable `professional` plan for specific feature in `pricingData`

--- home/src/content/pricing/pricingData.ts
+++ home/src/content/pricing/pricingData.ts
@@ -253,10 +253,10 @@ export const getPlanFeatureCategories = (
         availability: {
           basic: false,
           starter: false,
-          professional: false,
+          professional: true,
           business: true,
           "sh-free": false,
-          "sh-professional": false,
+          "sh-professional": true,
           "sh-business": true,
         },
       },
