5e601a41e15166b99c954b15560b0969812f3756
Ibrahima G. Coulibaly <ibracool99@gmail.com>
2026-04-07 16:34:07 +0100

    refactor: update `VendorRepository` query to return earliest matching vendor by creation date

--- api/src/main/java/com/grash/repository/VendorRepository.java
+++ api/src/main/java/com/grash/repository/VendorRepository.java
@@ -10,6 +10,17 @@ import java.util.Optional;
 public interface VendorRepository extends JpaRepository<Vendor, Long>, JpaSpecificationExecutor<Vendor> {
     Collection<Vendor> findByCompany_Id(Long id);
 
+<<<<<<< HEAD
+=======
+    @Query("""
+                SELECT MIN(v) 
+                FROM Vendor v
+                WHERE (LOWER(v.companyName) = LOWER(:name)
+                    OR LOWER(v.name) = LOWER(:name))
+                  AND v.company.id = :companyId
+                ORDER BY v.createdAt
+            """)
+>>>>>>> 0443a7f7 (refactor: update `VendorRepository` query to return earliest matching vendor by creation date)
     Optional<Vendor> findByNameIgnoreCaseAndCompany_Id(String name, Long companyId);
 
     void deleteByCompany_IdAndIsDemoTrue(Long companyId);
