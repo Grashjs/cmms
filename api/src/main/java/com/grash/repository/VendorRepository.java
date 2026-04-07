1a7682977061090ab675e9fbedca4480f9f0d7cb
semihsendogan-dotcom <semihsendogan@gmail.com>
2026-04-08 00:18:59 +0300

    1

--- api/src/main/java/com/grash/repository/VendorRepository.java
+++ api/src/main/java/com/grash/repository/VendorRepository.java
@@ -3,6 +3,7 @@ package com.grash.repository;
 import com.grash.model.Vendor;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Collection;
 import java.util.Optional;
@@ -10,8 +11,6 @@ import java.util.Optional;
 public interface VendorRepository extends JpaRepository<Vendor, Long>, JpaSpecificationExecutor<Vendor> {
     Collection<Vendor> findByCompany_Id(Long id);
 
-<<<<<<< HEAD
-=======
     @Query("""
                 SELECT MIN(v) 
                 FROM Vendor v
@@ -20,7 +19,6 @@ public interface VendorRepository extends JpaRepository<Vendor, Long>, JpaSpecif
                   AND v.company.id = :companyId
                 ORDER BY v.createdAt
             """)
->>>>>>> 0443a7f7 (refactor: update `VendorRepository` query to return earliest matching vendor by creation date)
     Optional<Vendor> findByNameIgnoreCaseAndCompany_Id(String name, Long companyId);
 
     void deleteByCompany_IdAndIsDemoTrue(Long companyId);
