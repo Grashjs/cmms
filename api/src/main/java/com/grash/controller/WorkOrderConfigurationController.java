47afbed1d82d1250605fcd96c0eb238a89d920d0
tomaioo <hongsaygio.com@gmail.com>
2026-04-21 17:15:33 -0700

    fix(security): 2 improvements across 2 files
    - Security: IDOR/Broken object-level authorization in WorkOrderConfiguration lookup
    - Security: IDOR/Broken object-level authorization in WorkOrderRequestConfiguration lookup
    
    Signed-off-by: tomaioo <203048277+tomaioo@users.noreply.github.com>

--- api/src/main/java/com/grash/controller/WorkOrderConfigurationController.java
+++ api/src/main/java/com/grash/controller/WorkOrderConfigurationController.java
@@ -29,13 +29,16 @@ public class WorkOrderConfigurationController {
     private final UserService userService;
 
     @GetMapping("/{id}")
-    @PreAuthorize("permitAll()")
+    @PreAuthorize("isAuthenticated()")
 
     public WorkOrderConfiguration getById(@PathVariable("id") Long id, HttpServletRequest req) {
         User user = userService.whoami(req);
         Optional<WorkOrderConfiguration> optionalWorkOrderConfiguration = workOrderConfigurationService.findById(id);
         if (optionalWorkOrderConfiguration.isPresent()) {
             WorkOrderConfiguration savedWorkOrderConfiguration = optionalWorkOrderConfiguration.get();
+            if (!savedWorkOrderConfiguration.getCompanySettings().getCompany().getId().equals(user.getCompany().getId())) {
+                throw new CustomException("Forbidden", HttpStatus.FORBIDDEN);
+            }
             return savedWorkOrderConfiguration;
         } else throw new CustomException("Not found", HttpStatus.NOT_FOUND);
     }
