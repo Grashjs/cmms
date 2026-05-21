52732853bb8490db2893a3ca50d17e86cc6cd884
Ibrahima G. Coulibaly <ibracool99@gmail.com>
2026-05-21 05:03:19 +0100

    fix: remove utm-tracker-hook dependency and clean up Providers component

--- home/src/components/Providers.tsx
+++ home/src/components/Providers.tsx
@@ -2,11 +2,8 @@
 
 import React, { ReactNode } from "react";
 import { SnackbarProvider } from "notistack";
-import { SidebarProvider } from "src/contexts/SidebarContext";
-import { TitleProvider } from "src/contexts/TitleContext";
 import { CustomSnackBarProvider } from "src/contexts/CustomSnackBarContext";
 import ThemeProvider from "src/theme/ThemeProvider";
-import { UtmTrackerProvider } from "@nik0di3m/utm-tracker-hook";
 
 interface ProvidersProps {
   children: ReactNode;
@@ -14,20 +11,16 @@ interface ProvidersProps {
 
 export default function Providers({ children }: ProvidersProps) {
   return (
-    <UtmTrackerProvider customParams={["msclkid"]}>
-      <TitleProvider>
-        <ThemeProvider>
-          <SnackbarProvider
-            maxSnack={6}
-            anchorOrigin={{
-              vertical: "bottom",
-              horizontal: "right",
-            }}
-          >
-            <CustomSnackBarProvider>{children}</CustomSnackBarProvider>
-          </SnackbarProvider>
-        </ThemeProvider>
-      </TitleProvider>
-    </UtmTrackerProvider>
+    <ThemeProvider>
+      <SnackbarProvider
+        maxSnack={6}
+        anchorOrigin={{
+          vertical: "bottom",
+          horizontal: "right",
+        }}
+      >
+        <CustomSnackBarProvider>{children}</CustomSnackBarProvider>
+      </SnackbarProvider>
+    </ThemeProvider>
   );
 }
