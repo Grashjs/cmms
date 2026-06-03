e4103d85de59da376650fa79afd2a1b932f90255
Ibrahima G. Coulibaly <ibracool99@gmail.com>
2026-06-03 17:17:29 +0100

    feat: replace FileShowDTO with FileThumbnailDTO for image fields across multiple DTOs

--- api/src/main/java/com/grash/dto/LocationShowDTO.java
+++ api/src/main/java/com/grash/dto/LocationShowDTO.java
@@ -2,7 +2,6 @@ package com.grash.dto;
 
 import com.grash.dto.cutomField.CustomFieldValueShowDTO;
 import com.grash.model.Company;
-import com.grash.dto.FileShowDTO;
 import com.grash.model.Location;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
@@ -47,7 +46,7 @@ public class LocationShowDTO extends AuditShowDTO {
     private List<UserMiniDTO> workers = new ArrayList<>();
 
     @Schema(description = "Image file associated with the location")
-    private FileShowDTO image;
+    private FileThumbnailDTO image;
 
     @Schema(description = "List of files attached to the location")
     private List<FileMiniDTO> files;
