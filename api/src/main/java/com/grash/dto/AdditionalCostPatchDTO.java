c28e589c74c140a12c1fe4d1785381866bcf14c8
Ibrahima G. Coulibaly <ibracool99@gmail.com>
2026-07-07 15:13:00 +0000

    fix: enhance AdditionalCostPatchDTO with new fields for cost inclusion and date

--- api/src/main/java/com/grash/dto/AdditionalCostPatchDTO.java
+++ api/src/main/java/com/grash/dto/AdditionalCostPatchDTO.java
@@ -1,9 +1,14 @@
 package com.grash.dto;
 
+import com.grash.model.CostCategory;
 import com.grash.model.User;
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.ManyToOne;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.Date;
 
 @Data
 @NoArgsConstructor
@@ -12,4 +17,12 @@ public class AdditionalCostPatchDTO {
     @Schema(implementation = IdDTO.class)
     private User assignedTo;
     private double cost;
+
+    @Schema(description = "Whether to include this cost in the total")
+    private boolean includeToTotalCost;
+
+    @Schema(description = "Date of the additional cost")
+    private Date date;
+
+    private CostCategory category;
 }
