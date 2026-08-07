72b9da8e07554be6d123c5b1bc135f946275f62e
Ibrahima G. Coulibaly <ibracool99@gmail.com>
2026-08-07 17:20:14 +0000

    refactor: remove unnecessary blank line in formatters.ts

--- frontend/src/utils/formatters.ts
+++ frontend/src/utils/formatters.ts
@@ -66,7 +66,6 @@ export const durationToHours = (duration: number) => {
   if (hrs > 0) {
     ret += '' + hrs + ':' + (mins < 10 ? '0' : '');
   }
-
   ret += '' + mins + ':' + (secs < 10 ? '0' : '');
   ret += '' + secs;
   return ret;
