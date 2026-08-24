463f65b63aa72b221e22052254bf81d609a78aa5
Ibrahima G. Coulibaly <ibracool99@gmail.com>
2026-08-24 14:02:54 +0000

    refactor: add timeout annotation to ApiDocsIntegrationTest for improved test reliability

--- api/src/test/java/com/grash/integration/ApiDocsIntegrationTest.java
+++ api/src/test/java/com/grash/integration/ApiDocsIntegrationTest.java
@@ -1,10 +1,13 @@
 package com.grash.integration;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.concurrent.TimeUnit;
 
 import static org.hamcrest.Matchers.greaterThan;
 import static org.hamcrest.Matchers.hasItem;
@@ -15,6 +18,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @AutoConfigureMockMvc(addFilters = false)
+@Timeout(value = 1, unit = TimeUnit.MINUTES)
 class ApiDocsIntegrationTest extends AbstractIntegrationTest {
 
     @Autowired
