67411140bffab516cc99ffdf9ee244912027bbd1
Ibrahima G. Coulibaly <ibracool99@gmail.com>
2026-08-31 14:19:54 +0000

    feat: add request and part assignment checks for user permissions

--- frontend/src/contexts/JWTAuthContext.tsx
+++ frontend/src/contexts/JWTAuthContext.tsx
@@ -17,6 +17,8 @@ import {
   FieldType
 } from '../models/owns/fieldConfiguration';
 import { Company } from '../models/owns/company';
+import Request from '../models/owns/request';
+import Part from '../models/owns/part';
 import { PermissionEntity } from 'src/models/owns/role';
 import { Audit } from 'src/models/owns/audit';
 import OwnSubscription from '../models/owns/ownSubscription';
@@ -966,6 +968,41 @@ export const AuthProvider: FC<AuthProviderProps> = (props) => {
         state.user.id === entity.createdBy ||
         state.user.role.editOtherPermissions.includes(permissionEntity) ||
         isAssignedTo(entity as unknown as Location, state.user)
+      );
+    } else if (permissionEntity === PermissionEntity.REQUESTS) {
+      const isAssignedTo = (request: Request, user: OwnUser): boolean => {
+        let users = [];
+        if (request.primaryUser) {
+          users.push(request.primaryUser);
+        }
+        if (request.team) {
+          users = users.concat(request.team.users);
+        }
+        if (request.assignedTo) {
+          users = users.concat(request.assignedTo);
+        }
+        return users.some((user1) => user1.id === user.id);
+      };
+      return (
+        state.user.id === entity.createdBy ||
+        state.user.role.editOtherPermissions.includes(permissionEntity) ||
+        isAssignedTo(entity as unknown as Request, state.user)
+      );
+    } else if (
+      permissionEntity === PermissionEntity.PARTS_AND_MULTIPARTS &&
+      'assignedTo' in entity
+    ) {
+      const isAssignedTo = (part: Part, user: OwnUser): boolean => {
+        let users = [];
+        if (part.assignedTo) {
+          users = users.concat(part.assignedTo);
+        }
+        return users.some((user1) => user1.id === user.id);
+      };
+      return (
+        state.user.id === entity.createdBy ||
+        state.user.role.editOtherPermissions.includes(permissionEntity) ||
+        isAssignedTo(entity as unknown as Part, state.user)
       );
     }
     return (
